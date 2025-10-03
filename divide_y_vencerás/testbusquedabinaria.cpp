9b2756e31a774ab62a995e596fc242c4f3516241
Cvives1 <cvivolm@etsinf.upv.es>
2025-10-03 10:50:16 +0200

    practica 2 completa corrigiendo errores 2.2

--- divide_y_vencerás/testbusquedabinaria.cpp
+++ divide_y_vencerás/testbusquedabinaria.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    // Ejemplo con vector ASCENDENTE
+    
     vector<int> asc{1, 3, 5, 7, 9, 11, 13, 15};
     int x1 = 7;
     int pos1 = BusquedaBinaria(x1, asc, 0, asc.size() - 1);
@@ -14,7 +14,7 @@ int main() {
     else
         cout << "ASC: El valor " << x1 << " NO está en el vector" << endl;
 
-    // Ejemplo con vector DESCENDENTE
+    
     vector<int> desc{20, 17, 14, 11, 8, 5, 2};
     int x2 = 11;
     int pos2 = BusquedaBinaria_INV(x2, desc, 0, desc.size() - 1);
@@ -24,11 +24,10 @@ int main() {
     else
         cout << "DESC: El valor " << x2 << " NO está en el vector" << endl;
 
-    // Ejemplo con vector de CHARS ascendente
+    
     vector<char> letras{'a', 'c', 'e', 'g', 'i'};
     char x3 = 'e';
-    // Para char haríamos una función genérica (plantilla), pero aquí mostramos ints
-    // Podrías convertir el char a int para buscarlo si solo tienes int
+    
     vector<int> letras_int(letras.begin(), letras.end());
     int pos3 = BusquedaBinaria((int)x3, letras_int, 0, letras_int.size() - 1);
 
